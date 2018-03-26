4914eb239247cad332666e718b67fb88bcd4f748
OlegMaksimov <maksimovoleg19@gmail.com>
2018-03-26 10:36:16 +0500

    Дописал вставку данных в таблицу. Добавил поле для имени таблицы. Написал для нее валидацию.

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -1,8 +1,12 @@
 package sample.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import oracle.jdbc.driver.OracleConnection;
+import sample.Connection.JdbcConnection;
 import sample.ImportFileFactory.ImporFileFactory;
 import sample.inputFileService.IImportFile;
 
@@ -14,6 +18,13 @@ public class Controller {
 
     static String F_PATH = null;
     static  IImportFile FILE;
+    OracleConnection connection = JdbcConnection.getInstance().getConnection();
+    @FXML
+    public Label filename;
+    @FXML
+    public Label lblWarn;
+    @FXML
+    public TextField tableName;
 
     @FXML
     public void openImportFile() throws Exception {
@@ -25,8 +36,10 @@ public class Controller {
       );
         Stage stage = new Stage();
         File selectedFile = fileChooser.showOpenDialog(stage);
+//        System.out.println(textField.getText());
         if (selectedFile != null) {
             F_PATH = selectedFile.getAbsolutePath();
+            filename.setText(selectedFile.getName());
         } else {
             throw new Exception("Can't open file");
         }
@@ -35,11 +48,18 @@ public class Controller {
     }
 
     @FXML
-    public void onClickImportData(){
+    public void onClickImportData() throws Exception {
 
-        if (!FILE.equals(null)){
-            List list = FILE.parsingFile("table2");
-            System.out.println(list.get(1));
+        List list;
+        if (!(FILE == null)){
+            if(!(tableName.getText().isEmpty())){ // TODO: 26.03.2018 разобраться
+             lblWarn.setVisible(false);
+                list = FILE.parsingFile(tableName.getText());
+             FILE.insertToDB(connection,list,tableName.getText());
+            }
+            else{
+              lblWarn.setVisible(true);
+            }
         }
     }
 }
